3d0008e31e566c5c75560f238e1ac92230e350d2
Ricky Bobby <richard.pajewski@gmail.com>
2021-02-05 16:32:16 -0600

    server changes started, directory and files in place for schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { AuthenticationError } = require('apollo-server-express')
+const { signToken } = require('../utils/auth')
+
+const { User } = require('../models')
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+        
+    }
+}
+
+module.exports = resolvers
