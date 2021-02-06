24ac47853ab50965d9fdb8d9afc1f25fa0a09728
Ricky Bobby <richard.pajewski@gmail.com>
2021-02-06 09:49:04 -0600

    dependencies added for gql and apollo imported, file structures added

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
+
+// pages
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
+
+// page components
+import Navbar from './components/Navbar'
+
+// set current users web token as header bearer and connect back-end
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token')
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+})
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  );
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
